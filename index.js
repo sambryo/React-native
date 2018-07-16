2a4338218673d0dd2744fa390aaf78b4f1f24509
samTer <samuelbryo@gmail.com>
2018-07-15 17:43:40 -0700

    starting view tag

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-/** @format */
-
 import React from 'react';
 import {Text, AppRegistry} from 'react-native';
+import Header from './src/components/header';
 
-const App = () => {
+const App = () => (
   return <Text> Some Text </Text>
-
-}
+);
 
 AppRegistry.registerComponent('albums', () => App);
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,19 @@
 // Import libraries for making a component
 import React from 'react'
-import ReactNative from 'react-native'
+import { Text, View } from 'react-native'
 // Make a component
 
+
+const Header = () => {
+  const { textStyle } = styles;
+  return <Text style={textStyle}> Albums! </Text>
+}
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+}
+
 // Make the component available to other parts of the app
+export default Header
